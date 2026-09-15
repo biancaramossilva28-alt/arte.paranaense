257370f710103daea3e41832c4f9864453c4e566
biancaramossilva28-alt <bianca.ramos.silva28@escola.pr.gov.br>
2026-09-15 11:16:05 -0300

    atualização de erros

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoesCuritir = document.queryelectorAll (".curtir");
+botoesCuritir.foreach(function(botoesCuritir)
+let curtiu=false;
+botoesCuritir.addEventLister("click", curtir);
+function curtir(){
+    const contador =botoesCuritir.queryelectorAll ("span");
+    if (curtiu === false){
+        contador.textContent++;
+        curtiu = true;}
+        else {
+            con.textContent
+            curtiu--;
+        }
+}
+)
 
